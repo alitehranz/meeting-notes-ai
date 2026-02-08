5f9824e711df4b58e35c7eec4e98643eb606baa1
Ali Tehrani <ali.tehrani586@gmail.com>
2026-02-09 03:16:38 +1000

    Update database config

--- app/models/database.py
+++ app/models/database.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 class Meeting(Base):
     __tablename__ = "meetings"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True) 
     title = Column(String, nullable=False)
     date = Column(DateTime, nullable=False)
     raw_notes = Column(Text, nullable=False)
